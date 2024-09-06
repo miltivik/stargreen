cfec49e26515b8213b54ff0e6dffd2a374c48019
miliS <134473229+miltivik@users.noreply.github.com>
2024-09-06 19:31:32 -0300

    haciendo email checker

--- models/User.cjs
+++ models/User.cjs
@@ -1,15 +1,32 @@
-const { name } = require('ejs');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
 
-const UserSchema = new Schema ({
-    name: String,
-    email: String,
-    password: String,
-    dateOfBirth: Date,
-    verified: Boolean
+const userSchema = new mongoose.Schema({
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true },
+	password: { type: String, required: true },
 });
 
-const User = mongoose.model('User', UserSchema);
+userSchema.methods.generateAuthToken = function () {
+	const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+		expiresIn: "7d",
+	});
+	return token;
+};
 
-module.exports = User;
+const User = mongoose.model("user", userSchema);
+
+const validate = (data) => {
+	const schema = Joi.object({
+		firstName: Joi.string().required().label("First Name"),
+		lastName: Joi.string().required().label("Last Name"),
+		email: Joi.string().email().required().label("Email"),
+		password: passwordComplexity().required().label("Password"),
+	});
+	return schema.validate(data);
+};
+
+module.exports = { User, validate };
